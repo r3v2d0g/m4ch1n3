ce9105ba28b88f6859e9200ebb855d542f1b4375
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-03 20:15:13 +0100

    users/root: refactor

--- users/root/default.nix
+++ users/root/default.nix
@@ -1,15 +1,22 @@
-{ config, lib, pkgs, ... }:
+{ lib, mcfg, pkgs, ... }:
 
-let cfg = config.m4ch1n3.users.root;
+let
+  cfg = mcfg.users.root;
+in {
+  options.m4ch1n3.users.root.enable = lib.mkOptBool true;
 
-in { options.m4ch1n3.users.root =
-       { enable = lib.mkDisableOption "root"; };
+  config = lib.mkIf cfg.enable {
+    home-manager.users.root.m4ch1n3 = {
+      comm.enable = false;
+      editor.emacs.enable = true;
+      wm.enable = false;
+    };
 
-     config = lib.mkIf cfg.enable
-       { home-manager.users.root =
-           { m4ch1n3.editor.emacs.enable = true; };
+    users.users.root = {
+      hashedPassword =
+        "$6$rounds=100000$.iwGNU2p46IK0Fbo$YlfkQxalWM9vC3iyEhh3hJLv1MhqTF2PoanBenl5IpTENfdih2Y2oc.0xfIywy1MKLPKdDQ43V92cNz2sntfP.";
 
-         users.users.root.hashedPassword =
-            "$6$rounds=100000$.iwGNU2p46IK0Fbo$YlfkQxalWM9vC3iyEhh3hJLv1MhqTF2PoanBenl5IpTENfdih2Y2oc.0xfIywy1MKLPKdDQ43V92cNz2sntfP.";
-       };
-   }
+      shell = pkgs.zsh;
+    };
+  };
+}
