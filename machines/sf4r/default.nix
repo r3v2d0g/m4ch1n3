5ba369b6d5a9ebd952e5693738d1e0dc1454e8c7
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-03 20:16:45 +0100

    machines/sf4r: refactor

--- machines/sf4r/default.nix
+++ machines/sf4r/default.nix
@@ -1,48 +1,53 @@
 { config, pkgs, ... }:
 
-{ base =
-    { boot.device = "/dev/disk/by-id/ata-INTEL_SSDSCKKF256G8H_BTLA7452075L256J";
-      boot.packages = [ pkgs.rtl8723de ];
+{
+  base = {
+    boot.device = "/dev/disk/by-id/ata-INTEL_SSDSCKKF256G8H_BTLA7452075L256J";
+    boot.modules = [ "rtl8723de" ];
+    boot.packages = [ pkgs.rtl8723de ];
 
-      luks.devices."sf4r" =
-        { device = "/dev/disk/by-partlabel/sf4r"; };
+    luks.devices."sf4r".device = "/dev/disk/by-partlabel/sf4r";
 
-      fs.boot = "/dev/disk/by-uuid/CBCB-4D7F";
+    fs.boot = "/dev/disk/by-uuid/CBCB-4D7F";
 
-      net.host.name = "sf4r";
-      net.host.id = "41b22822";
-      net.interfaces = [ "eno1" ];
-    };
+    net.host.name = "sf4r";
+    net.host.id = "41b22822";
+    net.interfaces = [ "eno1" ];
+    net.netman = true;
+    net.idevice = true;
+  };
 
-  gpg.yubikey = true;
-  security.yubico.enable = true;
+  dev.enable = true;
 
-  security.ssh =
-    { enable = true;
-      address = "192.168.1.4";
-    };
+  security = {
+    yubico.enable = true;
 
-  users.r3v2d0g =
-    { enable = true;
-      password = "local";
+    ssh.server = true;
+    ssh.addr = "192.168.1.2";
+  };
 
-      comm.enable = true;
-      security.pass.enable = true;
+  users.eqs.enable = true;
+  users.r3v2d0g = {
+    enable = true;
+    password = "local";
 
-      wm.autostart.enable = true;
-      wm.onepassword.enable = true;
-    };
+    comm.enable = true;
+    comm.discord = true;
+    comm.slack = true;
+    dev.cypress = true;
+    wm.autostart = true;
+  };
 
-  wm =
-    { enable = true;
+  wm = {
+    enable = true;
 
-      primary = "eDP-1";
-      outputs."eDP-1" =
-        { resolution.width = 1920;
-          resolution.height = 1080;
+    primary = "eDP-1";
+    outputs."eDP-1" = {
+      resolution.width = 1920;
+      resolution.height = 1080;
 
-          position.x = 0;
+      position.x = 0;
           position.y = 0;
-        };
     };
+  };
 }
