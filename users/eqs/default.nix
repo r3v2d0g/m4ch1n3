339608a8c06c4c5c82939b60a420e50e6916dac8
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-03 20:14:20 +0100

    users/eqs: create

---
+++ users/eqs/default.nix
@@ -0,0 +1,23 @@
+{ lib, mcfg, ... }:
+
+let
+  cfg = mcfg.users.eqs;
+  enable = mcfg.dev.enable && mcfg.dev.docker.enable;
+in {
+  options.m4ch1n3.users.eqs = lib.optionalAttrs enable { enable = lib.mkOptBool false; };
+
+  config = lib.mkIf (enable && cfg.enable) {
+    home-manager.users.eqs.m4ch1n3 = {
+      dev.docker.enable = true;
+      editor.emacs.enable = false;
+    };
+
+    users.extraUsers.eqs = {
+      uid = 2000;
+      group = "users";
+      extraGroups = [ "eqs" "docker" ];
+    };
+
+    users.extraGroups.eqs.gid = 2000;
+  };
+}
