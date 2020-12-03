a3dba6a085c6854405eba9f03f5d3e39cd8a7f77
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-03 20:14:30 +0100

    users/r3v2d0g: refactor

--- users/r3v2d0g/default.nix
+++ users/r3v2d0g/default.nix
@@ -1,184 +1,147 @@
-{ config, lib, pkgs, ... }:
+{ lib, pkgs, mcfg, ... }:
 
-let cfg = config.m4ch1n3.users.r3v2d0g;
-    mcfg = config.m4ch1n3;
+let
+  cfg = mcfg.users.r3v2d0g;
+in {
+  options.m4ch1n3.users.r3v2d0g = {
+    enable = lib.mkOptBool true;
 
-in { options.m4ch1n3.users.r3v2d0g =
-       { enable = lib.mkEnableOption "r3v2d0g";
+    name = lib.mkOptStr "Matthieu Le brazidec (r3v2d0g)";
+    email = lib.mkOptStr "r3v2d0g@jesus.gg";
 
-         name = lib.mkStrOption
-           { default = "Matthieu Le brazidec (r3v2d0g)"; };
+    uid = lib.mkOptInt 1;
 
-         email = lib.mkStrOption
-           { default = "r3v2d0g@jesus.gg"; };
+    wheel = lib.mkOptBool true;
+    groups = lib.mkOptStrList [];
 
-         uid = lib.mkIntOption
-           { default = 1; };
+    home.path = lib.mkOptStr "/home/r3v2d0g";
+    home.create = lib.mkOptBool true;
 
-         wheel = lib.mkDisableOption "wheel group";
+    password = lib.mkOptEnum [ "local" "remote" ] null;
 
-         groups = lib.mkListOfStrOption
-           { default = []; };
+    keys.git = lib.mkOptStr "0x14D54AAE149D1DC9";
+    keys.ssh = lib.mkOptStrList
+      [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWlbkVr1wDQIkEcgXOrBAMBSE+W5UR2uvL4Sj9mMJoc" ];
 
-         home.path = lib.mkStrOption
-           { default = "/home/r3v2d0g"; };
+    comm = {
+      enable = lib.mkOptBool false;
 
-         home.create = lib.mkDisableOption "create home directory";
+      discord = lib.mkOptBool false;
+      pm = lib.mkOptBool false;
+      slack = lib.mkOptBool false;
+    };
 
-         password = lib.mkEnumOption [ "local" "remote" ] {};
+    dev = lib.optionalAttrs mcfg.dev.enable {
+      enable = lib.mkOptBool true;
 
-         keys.ssh = lib.mkListOfStrOption
-           { default =
-               [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWlbkVr1wDQIkEcgXOrBAMBSE+W5UR2uvL4Sj9mMJoc" ];
-           };
+      cypress = lib.mkOptBool false;
+      docker = lib.mkOptBool true;
 
-         keys.git = lib.mkStrOption
-           { default = "0x14D54AAE149D1DC9"; };
+      haskell = lib.mkOptBool true;
+      nodejs = lib.mkOptBool true;
+      python = lib.mkOptBool true;
+      rust = lib.mkOptBool true;
+      typescript = lib.mkOptBool true;
+      vuejs = lib.mkOptBool true;
+    };
 
-         comm = { enable = lib.mkEnableOption "communication"; };
+    wm = lib.optionalAttrs mcfg.wm.enable {
+      enable = lib.mkOptBool true;
 
-         dev =
-           { docker = lib.optionalAttrs mcfg.dev.docker.enable
-               { enable = lib.mkDisableOption "docker"; };
+      autostart = lib.mkOptBool false;
 
-             jetbrains = lib.optionalAttrs mcfg.wm.enable
-               { enable = lib.mkDisableOption "JetBrains tools";
+      audio = lib.mkOptBool true;
+      browser = lib.mkOptBool true;
+      onepassword = lib.mkOptBool true;
+      term = lib.mkOptBool true;
+    };
+  };
 
-                 webstorm.enable = lib.mkDisableOption "WebStorm";
-                 webstorm.eap = lib.mkDisableOption "EAP";
-               };
-           };
+  config = lib.mkIf cfg.enable {
+    home-manager.users.r3v2d0g = {
+      programs.git = {
+        userName = cfg.name;
+        userEmail = cfg.email;
 
-         security.pass =
-           { enable = lib.mkEnableOption "password-store"; };
+        signing.key = cfg.keys.git;
+        signing.signByDefault = true;
+      };
 
-         wm = lib.optionalAttrs mcfg.wm.enable
-           { enable = lib.mkDisableOption "window manager";
+      m4ch1n3.comm = { enable = cfg.comm.enable; }
+                     // lib.optionalAttrs cfg.comm.enable {
+                       discord.enable = cfg.comm.discord;
+                       pm.enable = cfg.comm.pm;
+                       slack.enable = cfg.comm.slack;
+                     };
 
-             autostart =
-               { enable = lib.mkEnableOption "autostart";
+      m4ch1n3.dev = lib.mkIf mcfg.dev.enable
+        ({ enable = cfg.dev.enable; }
+         // lib.optionalAttrs cfg.dev.enable {
+           cypress.enable = lib.mkIf (mcfg.wm.enable && cfg.wm.enable) cfg.dev.cypress;
+           docker.enable = lib.mkIf mcfg.dev.docker.enable cfg.dev.docker;
 
-                 tty = lib.mkStrOption
-                   { default = "/dev/tty1"; };
-               };
+           haskell.enable = cfg.dev.haskell;
+           nodejs.enable = cfg.dev.nodejs;
+           python.enable = cfg.dev.python;
+           rust.enable = cfg.dev.rust;
+           typescript.enable = cfg.dev.typescript;
+           vuejs.enable = cfg.dev.vuejs;
+         });
 
-             audio = lib.optionalAttrs mcfg.wm.audio.enable
-               { enable = lib.mkDisableOption "audio"; };
+      m4ch1n3.security = {
+        gpg.agent.enable = true;
+        pass.enable = true;
+      };
 
-             browser = lib.optionalAttrs mcfg.wm.browser.enable
-               { enable = lib.mkDisableOption "web browser"; };
+      m4ch1n3.wm = lib.mkIf mcfg.wm.enable
+        ({ enable = cfg.wm.enable; }
+         // lib.optionalAttrs cfg.wm.enable {
+           autostart.enable = cfg.wm.autostart;
 
-             onepassword = { enable = lib.mkEnableOption "1password"; };
+           audio.enable = lib.mkIf mcfg.wm.audio.enable cfg.wm.audio;
+           browser.enable = lib.mkIf mcfg.wm.browser.enable cfg.wm.browser;
+           onepassword.enable = cfg.wm.onepassword;
+           term.enable = cfg.wm.term;
+         });
+    };
 
-             term = { enable = lib.mkDisableOption "terminal"; };
-           };
-       };
+    users.extraUsers.r3v2d0g = {
+      description = cfg.name;
 
-     config = lib.mkIf cfg.enable
-       { home-manager.users.r3v2d0g =
-           { m4ch1n3.comm.enable = cfg.comm.enable;
+      uid = cfg.uid;
 
-             m4ch1n3.dev =
-               { docker.enable = lib.mkIf mcfg.dev.docker.enable cfg.dev.docker.enable;
+      group = "users";
+      extraGroups = cfg.groups
+                    ++ lib.optional cfg.wheel "wheel"
+                    ++ lib.optional (mcfg.dev.enable && mcfg.dev.docker.enable
+                                     && cfg.dev.docker) "docker";
 
-                 jetbrains = lib.mkIf (mcfg.wm.enable && cfg.wm.enable)
-                   { enable = cfg.dev.jetbrains.enable;
+      home = cfg.home.path;
+      createHome = cfg.home.create;
 
-                     webstorm.enable = cfg.dev.jetbrains.webstorm.enable;
-                     webstorm.eap = cfg.dev.jetbrains.webstorm.eap;
-                   };
-               };
+      hashedPassword =
+        if cfg.password == "local" then
+          "$6$rounds=100000$oIBVnrx1.haJBM$uaaQQMmQ/i0Fums1lPZEmdZnxTrkz9M3sOziOY08XUBeil0v5G8k6qJ.jcIgBSY7Azf7KAdAHHR3WG3jSIqy5."
+        else
+          if cfg.password == "remote" then
+            "$6$rounds=100000$9u6AJvu8NJRMh9qI$77PgODhqPiuy2mnFsW7rzh7d1Ahj5cbqummDlhb9M8p7NuI1bKx4KRPN1RgVJlZ.gaE1tcG/CTRSjuTrbrEW/1"
+          else "";
 
-             m4ch1n3.editor.emacs =
-               { enable = true;
+      shell = pkgs.zsh;
 
-                 #config =
-                 #  { user.name = cfg.name;
-                 #    user.email = cfg.email;
+      openssh.authorizedKeys.keys = cfg.keys.ssh;
+    };
 
-                 #    setq =
-                 #      { "auth-sources" =
-                 #          ''
-                 #             '("~/.autoinfo.gpg")
-                 #          '';
-
-                 #        "lsp-enable-semantic-highlighting" = "t";
-                 #        # TODO: "lsp-rust-analyzer-cargo-watch-command" = ''"clippy"'';
-                 #        "lsp-rust-analyzer-server-display-inlay-hints" = "t";
-                 #        "lsp-rust-analyzer-display-parameter-hints" = "t";
-                 #        "lsp-rust-analyzer-display-chaining-hints" = "t";
-
-                 #        "rustic-lsp-server" = "'rust-analyzer";
-                 #        "rustic-match-angle-brackets" = "nil";
-                 #      };
-                 #  };
-               };
-
-             programs.git =
-               { userName = cfg.name;
-                 userEmail = cfg.email;
-
-                 signing.key = cfg.keys.git;
-                 signing.signByDefault = true;
-               };
-
-             m4ch1n3.gpg.agent.enable = true;
-
-             m4ch1n3.security.pass.enable = cfg.security.pass.enable;
-
-             m4ch1n3.wm = lib.mkIf (mcfg.wm.enable && cfg.wm.enable)
-               { enable = true;
-
-                 autostart.enable = cfg.wm.autostart.enable;
-                 autostart.tty = cfg.wm.autostart.tty;
-
-                 audio = lib.mkIf mcfg.wm.audio.enable
-                   { enable = cfg.wm.audio.enable; };
-
-                 browser = lib.mkIf mcfg.wm.browser.enable
-                   { enable = cfg.wm.browser.enable; };
-
-                 onepassword.enable = cfg.wm.onepassword.enable;
-                 term.enable = cfg.wm.term.enable;
-               };
-           };
-
-         m4ch1n3.wm = lib.mkIf (mcfg.wm.enable && cfg.wm.enable)
-           { browser.extensions = lib.mkIf (mcfg.wm.browser.enable)
-               [ "cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx" # uBlock Origin
-                 "ogfcmafjalglgifnmanfmnieipoejdcf;https://clients2.google.com/service/update2/crx" # uMatrix
-                 "doojmbjmlfjjnbmnoijecmcbfeoakpjm;https://clients2.google.com/service/update2/crx" # NoScript
-                 "aeblfdkhhhdcdjpifhhbdiojplfjncoa;https://clients2.google.com/service/update2/crx" # 1Password X
-                 "bigefpfhnfcobdlfbedofhhaibnlghod;https://clients2.google.com/service/update2/crx" # MEGA
-                 "ennpfpdlaclocpomkiablnmbppdnlhoh;https://clients2.google.com/service/update2/crx" # Rust Search Extension
-                 "npeicpdbkakmehahjeeohfdhnlpdklia;https://clients2.google.com/service/update2/crx" # WebRTC Network Limiter
-                 "nkgllhigpcljnhoakjkgaieabnkmgdkb;https://clients2.google.com/service/update2/crx" # Don't Fuck With Paste
-                 "godiecgffnchndlihlpaajjcplehddca;https://clients2.google.com/service/update2/crx" # GhostText
-               ];
-           };
-
-         users.extraUsers.r3v2d0g =
-           { description = cfg.name;
-
-             uid = cfg.uid;
-             group = "users";
-             extraGroups = cfg.groups
-                           ++ lib.optional cfg.wheel "wheel"
-                           ++ lib.optional
-                             (mcfg.dev.docker.enable && cfg.dev.docker.enable)
-                             "docker";
-
-             home = cfg.home.path;
-             createHome = cfg.home.create;
-
-             hashedPassword =
-               if cfg.password == "local"
-               then "$6$rounds=100000$oIBVnrx1.haJBM$uaaQQMmQ/i0Fums1lPZEmdZnxTrkz9M3sOziOY08XUBeil0v5G8k6qJ.jcIgBSY7Azf7KAdAHHR3WG3jSIqy5."
-               else if cfg.password == "remote"
-               then "$6$rounds=100000$9u6AJvu8NJRMh9qI$77PgODhqPiuy2mnFsW7rzh7d1Ahj5cbqummDlhb9M8p7NuI1bKx4KRPN1RgVJlZ.gaE1tcG/CTRSjuTrbrEW/1"
-               else "";
-
-             openssh.authorizedKeys.keys = cfg.keys.ssh;
-           };
-       };
-   }
+    m4ch1n3.wm.browser.extensions = lib.mkIf (mcfg.wm.enable && cfg.wm.enable) [
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx" # uBlock Origin
+      "ogfcmafjalglgifnmanfmnieipoejdcf;https://clients2.google.com/service/update2/crx" # uMatrix
+      "doojmbjmlfjjnbmnoijecmcbfeoakpjm;https://clients2.google.com/service/update2/crx" # NoScript
+      "aeblfdkhhhdcdjpifhhbdiojplfjncoa;https://clients2.google.com/service/update2/crx" # 1Password X
+      "bigefpfhnfcobdlfbedofhhaibnlghod;https://clients2.google.com/service/update2/crx" # MEGA
+      "ennpfpdlaclocpomkiablnmbppdnlhoh;https://clients2.google.com/service/update2/crx" # Rust Search Extension
+      "npeicpdbkakmehahjeeohfdhnlpdklia;https://clients2.google.com/service/update2/crx" # WebRTC Network Limiter
+      "nkgllhigpcljnhoakjkgaieabnkmgdkb;https://clients2.google.com/service/update2/crx" # Don't Fuck With Paste
+      "godiecgffnchndlihlpaajjcplehddca;https://clients2.google.com/service/update2/crx" # GhostText
+    ];
+  };
+}
