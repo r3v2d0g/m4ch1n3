6d1f6c8c2d48410a6fecb075d51578c0a2e21a8e
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-03 20:08:42 +0100

    lib/term: refactor

--- lib/term/default.nix
+++ lib/term/default.nix
@@ -1,106 +1,81 @@
 { lib }:
 
-{ fmt =
-    { reset = ''\e[0m'';
+{
+  fmt = {
+    reset = ''\e[0m'';
 
-      it = ''\e[3m'';
+    weight.bold = ''\e[1m'';
+    weight.light = ''\e[2m'';
+    weight.normal = ''\e[22m'';
 
-      weight =
-        { bold = ''\e[1m'';
-          light = ''\e[2m'';
-          normal = ''\e[22m'';
-        };
+    italic.on = ''\e[3m'';
+    italic.off = ''\e[23m'';
 
-      italic =
-        { on = ''\e[3m'';
-          off = ''\e[23m'';
-        };
+    underline.on = ''\e[4m'';
+    underline.double = ''\e[21m'';
+    underline.off = ''\e[24m'';
 
-      underline =
-        { on = ''\e[4m'';
-          double = ''\e[21m'';
-          off = ''\e[24m'';
-        };
+    blink.slow = ''\e[5m'';
+    blink.fast = ''\e[6m'';
+    blink.off = ''\e[25m'';
 
-      blink =
-        { slow = ''\e[5m'';
-          fast = ''\e[6m'';
-          off = ''\e[25m'';
-        };
+    rev.on = ''\e[7m'';
+    rev.off = ''\e[27m'';
 
-      rev =
-        { on = ''\e[7m'';
-          off = ''\e[27m'';
-        };
+    hide.on = ''\e[8m'';
+    hide.off = ''\e[28m'';
 
-      hide =
-        { on = ''\e[8m'';
-          off = ''\e[28m'';
-        };
+    strike.on = ''\e[9m'';
+    strike.off = ''\e[29m'';
 
-      strike =
-        { on = ''\e[9m'';
-          off = ''\e[29m'';
-        };
+    fg.set = c: ''\e[${toString (c + 30)}m'';
+    fg.default = ''\e[39m'';
 
-      fg =
-        { set = c: ''\e[${toString (c + 30)}m'';
-          default = ''\e[39m'';
-        };
+    bg.set = c: ''\e[${toString (c + 40)}m'';
+    bg.default = ''\e[49m'';
 
-      bg =
-        { set = c: ''\e[${toString (c + 40)}m'';
-          default = ''\e[49m'';
-        };
+    font.default = ''\e[10m'';
+    font.alt = n: ''\e[${toString (n + 10)}m'';
+  };
 
-      font.default = ''\e[10m'';
-      font.alt = n: ''\e[${toString (n + 10)}m'';
-    };
+  colors = {
+    black = 0;
+    red = 1;
+    green = 2;
+    yellow = 3;
+    blue = 4;
+    magenta = 5;
+    cyan = 6;
+    white = 7;
+  };
 
-  colors =
-    { black = 0;
-      red = 1;
-      green = 2;
-      yellow = 3;
-      blue = 4;
-      magenta = 5;
-      cyan = 6;
-      white = 7;
-    };
+  cursor = {
+    up = n: ''\e[${toString n}A'';
+    down = n: ''\e[${toString n}B'';
+    forward = n: ''\e[${toString n}C'';
+    back = n: ''\e[${toString n}D'';
 
-  cursor =
-    { up = n: ''\e[${toString n}A'';
-      down = n: ''\e[${toString n}B'';
-      forward = n: ''\e[${toString n}C'';
-      back = n: ''\e[${toString n}D'';
+    next = n: ''\e[${toString n}E'';
+    prev = n: ''\e[${toString n}F'';
 
-      next = n: ''\e[${toString n}E'';
-      prev = n: ''\e[${toString n}F'';
+    col = c: ''\e[${toString c}G'';
+    pos = r: c: ''\e[${toString r};${toString c}H'';
 
-      col = c: ''\e[${toString c}G'';
-      pos = r: c: ''\e[${toString r};${toString c}H'';
+    save = ''\e[s'';
+    restore = ''\e[u'';
+  };
 
-      save = ''\e[s'';
-      restore = ''\e[u'';
-    };
+  clear = {
+    screen.toEnd = ''\e[0J'';
+    screen.toBeg = ''\e[1J'';
+    screen.vis = ''\e[2J'';
+    screen.all = ''\e[3J'';
 
-  clear =
-    { screen =
-        { toEnd = ''\e[0J'';
-          toBeg = ''\e[1J'';
-          vis = ''\e[2J'';
-          all = ''\e[3J'';
-        };
+    line.toEnd = ''\e[0K'';
+    line.toBeg = ''\e[1K'';
+    line.all = ''\e[2K'';
+  };
 
-      line =
-        { toEnd = ''\e[0K'';
-          toBeg = ''\e[1K'';
-          all = ''\e[2K'';
-        };
-    };
-
-  scroll =
-    { up = n: ''\e[${toString n}S'';
-      down = n: ''\e[${toString n}T'';
-    };
+  scroll.up = n: ''\e[${toString n}S'';
+  scroll.down = n: ''\e[${toString n}T'';
 }
