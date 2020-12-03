3af430430412bacca062d68344c4a1c94628fe40
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-03 20:16:36 +0100

    machines/a5k4: refactor

--- machines/a5k4/default.nix
+++ machines/a5k4/default.nix
@@ -1,82 +1,85 @@
 { config, ... }:
 
-{ base =
-    { boot.device = "/dev/disk/by-id/ata-HGST_HTS721010A9E630_JR10006P0RLBSF";
-      boot.modules = [ "wl" ];
-      boot.packages = [ config.boot.kernelPackages.broadcom_sta ];
+{
+  base = {
+    boot.device = "/dev/disk/by-id/ata-HGST_HTS721010A9E630_JR10006P0RLBSF";
+    boot.modules = [ "wl" ];
+    boot.packages = [ config.boot.kernelPackages.broadcom_sta ];
 
-      luks.keys."a5k4-keys".device = "/dev/disk/by-partlabel/a5k4-keys";
+    luks.keys."a5k4-keys".device = "/dev/disk/by-partlabel/a5k4-keys";
 
-      luks.devices."a5k4-part-1" =
-        { device = "/dev/disk/by-partlabel/a5k4-part-1";
+    luks.devices."a5k4-part-1" = {
+      device = "/dev/disk/by-partlabel/a5k4-part-1";
 
-          key.name = "a5k4-keys";
-          key.size = 4096;
-          key.offset = 0;
-        };
-
-      luks.devices."a5k4-part-2" =
-        { device = "/dev/disk/by-partlabel/a5k4-part-2";
-
-          key.name = "a5k4-keys";
-          key.size = 4096;
-          key.offset = 4096;
-        };
-
-      fs.boot = "/dev/disk/by-uuid/436A-302E";
-
-      net.host.name = "a5k4";
-      net.host.id = "a0abe6f6";
-      net.interfaces = [ "enp60s0" ];
+      key.name = "a5k4-keys";
+      key.size = 4096;
+      key.offset = 0;
     };
 
-  gpg.yubikey = true;
-  security.yubico.enable = true;
+    luks.devices."a5k4-part-2" = {
+      device = "/dev/disk/by-partlabel/a5k4-part-2";
 
-  security.ssh =
-    { enable = true;
-      address = "192.168.1.2";
+      key.name = "a5k4-keys";
+      key.size = 4096;
+      key.offset = 4096;
     };
 
-  users.r3v2d0g =
-    { enable = true;
-      password = "local";
+    fs.boot = "/dev/disk/by-uuid/436A-302E";
 
-      comm.enable = true;
-      security.pass.enable = true;
+    net.host.name = "a5k4";
+    net.host.id = "a0abe6f6";
+    net.interfaces = [ "enp60s0" ];
+  };
 
-      wm.autostart.enable = true;
-      wm.onepassword.enable = true;
+  dev.enable = true;
+
+  security = {
+    yubico.enable = true;
+
+    ssh.server = true;
+    ssh.addr = "192.168.1.2";
+  };
+
+  users.eqs.enable = true;
+  users.r3v2d0g = {
+    enable = true;
+    password = "local";
+
+    comm.enable = true;
+    comm.discord = true;
+    comm.slack = true;
+    dev.cypress = true;
+    wm.autostart = true;
+  };
+
+  wm = {
+    enable = true;
+
+    primary = "DP-1";
+    outputs."DP-1" = {
+      resolution.width = 3840;
+      resolution.height = 2160;
+
+      position.x = 0;
+      position.y = 0;
     };
 
-  wm =
-    { enable = true;
+    outputs."HDMI-A-2" = {
+      resolution.width = 1920;
+      resolution.height = 1080;
 
-      primary = "DP-1";
-      outputs."DP-1" =
-        { resolution.width = 3840;
-          resolution.height = 2160;
+      rotation = 90;
 
-          position.x = 0;
-          position.y = 0;
-        };
-
-      outputs."HDMI-A-2" =
-        { resolution.width = 1920;
-          resolution.height = 1080;
-
-          rotation = 90;
-
-          position.x = 3840;
-          position.y = 2160 - 1920;
-        };
-
-      outputs."eDP-1" =
-        { resolution.width = 1920;
-          resolution.height = 1080;
-
-          position.x = 3840 / 2 - 1920 / 2;
-          position.y = 2160;
-        };
+      position.x = 3840;
+      position.y = 2160 - 1920;
     };
+
+    outputs."eDP-1" = {
+      resolution.width = 1920;
+      resolution.height = 1080;
+
+      position.x = 3840 / 2 - 1920 / 2;
+      position.y = 2160;
+    };
+  };
 }
