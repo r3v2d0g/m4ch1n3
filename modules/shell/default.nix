4a6e7b59cd90fcdae72df7ae7bdbe2dabf3b5759
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-03 20:12:11 +0100

    modules/shell: refactor

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -1,96 +1,99 @@
-{ machine = { ... }: {};
+{
+  machine = { ... }: {};
 
-  users = { config, inputs, lib, pkgs, ... }:
-    { home.packages =
-        [ pkgs.bat
-          pkgs.bottom
-          pkgs.du-dust
-          pkgs.exa
-          pkgs.fzf
-          pkgs.htop
-          pkgs.jq
-          pkgs.p7zip
-          pkgs.wget
-        ];
+  users = { inputs, lib, mcfg, pkgs, ucfg, ... }: {
+    home.packages = [
+      pkgs.bat
+      pkgs.bottom
+      pkgs.du-dust
+      pkgs.exa
+      pkgs.fzf
+      pkgs.htop
+      pkgs.jq
+      pkgs.p7zip
+      pkgs.wget
+    ];
 
-      programs.zsh =
-        { enable = true;
-          enableAutosuggestions = true;
-          enableCompletion = true;
-
-          autocd = true;
-          defaultKeymap = "emacs";
-
-          history.ignoreDups = true;
-          history.ignoreSpace = true;
-
-          oh-my-zsh =
-            { enable = true;
-              plugins = [ "sudo" ];
-            };
-
-          plugins =
-            [ { name = "jq-zsh-plugin";
-                file = "jq.plugin.zsh";
-                src = inputs.jq-zsh-plugin;
-              }
-
-              { name = "zsh-autopair";
-                file = "autopair.plugin.zsh";
-                src = inputs.zsh-autopair;
-              }
-
-              { name = "zsh-bd";
-                file = "bd.zsh";
-                src = inputs.zsh-bd;
-              }
-
-              { name = "zsh-nix-shell";
-                file = "nix-shell.plugin.zsh";
-                src = inputs.zsh-nix-shell;
-              }
-
-              { name = "zsh-syntax-highlighting";
-                src = inputs.zsh-syntax-highlighting;
-              }
-            ];
-
-          envExtra =
-            ''
-               PATH="$HOME/.yarn/bin:$PATH";
-            '';
-
-          initExtraBeforeCompInit =
-            ''
-               setopt PROMPT_SUBST
-            '';
-
-          sessionVariables =
-            { "PROMPT"  = " %B%F{11}%n%f@%F{13}%M%f:%F{12}%3~ %f%b ";
-              "RPROMPT" = "%F{12}\\\${IN_NIX_SHELL[1]} %(?.%F{7}.%F{9})\\$(printf \"%03d\" \\$?)%f ";
-
-              "ALTERNATE_EDITOR" = "emacs";
-              "EDITOR" = "emacsclient -c";
-
-              "ZSH_AUTOSUGGEST_USE_ASYNC" = true;
-              "ZSH_AUTOSUGGEST_STRATEGY" = [ "match_prev_cmd" "completion" ];
-              "ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE" = "fg=7";
-            };
-
-          shellAliases =
-            { "c" = "clear";
-              "em" = "emacs";
-              "emc" = "emacsclient --no-wait";
-              "emt" = "emacs -nw";
-
-              "du" = "${pkgs.du-dust}/bin/dust";
-              "l"    = "${pkgs.exa}/bin/exa -l";
-              "ls"   = "${pkgs.exa}/bin/exa";
-              "quit" = "exit";
-              "tree" = "${pkgs.exa}/bin/exa -T";
-            } // lib.optionalAttrs
-              (config.m4ch1n3.wm.enable && config.m4ch1n3.wm.term.enable)
-              { "icat" = "kitty +kitten icat"; };
-        };
+    home.sessionVariables = {
+      "ALTERNATE_EDITOR" = "emacs";
+      "EDITOR" = "emacsclient -c";
     };
+
+    programs.zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+
+      autocd = true;
+      defaultKeymap = "emacs";
+
+      history.ignoreDups = true;
+      history.ignoreSpace = true;
+
+      oh-my-zsh.enable = true;
+
+      plugins = [
+        {
+          name = "jq-zsh-plugin";
+          file = "jq.plugin.zsh";
+          src = inputs.jq-zsh-plugin;
+        }
+
+        {
+          name = "zsh-autopair";
+          file = "autopair.plugin.zsh";
+          src = inputs.zsh-autopair;
+        }
+
+        {
+          name = "zsh-bd";
+          file = "bd.zsh";
+          src = inputs.zsh-bd;
+        }
+
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = inputs.zsh-nix-shell;
+        }
+
+        {
+          name = "zsh-syntax-highlighting";
+          src = inputs.zsh-syntax-highlighting;
+        }
+      ];
+
+      envExtra = ''
+        PATH="$HOME/.yarn/bin:$PATH";
+      '';
+
+      initExtraBeforeCompInit = ''
+        setopt PROMPT_SUBST
+      '';
+
+      sessionVariables = {
+        "PROMPT"  = " %B%F{11}%n%f@%F{13}%M%f:%F{12}%3~ %f%b ";
+        "RPROMPT" = "%F{12}\\\${IN_NIX_SHELL[1]} %(?.%F{7}.%F{9})\\$(printf \"%03d\" \\$?)%f ";
+
+        "ZSH_AUTOSUGGEST_USE_ASYNC" = true;
+        "ZSH_AUTOSUGGEST_STRATEGY" = [ "match_prev_cmd" "completion" ];
+        "ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE" = "fg=7";
+      };
+
+      shellAliases = {
+        "c" = "clear";
+        "em" = "emacs";
+        "emc" = "emacsclient --no-wait";
+        "emt" = "emacs -nw";
+
+        "du" = "${pkgs.du-dust}/bin/dust";
+        "l"    = "${pkgs.exa}/bin/exa -l";
+        "ls"   = "${pkgs.exa}/bin/exa";
+        "quit" = "exit";
+        "tree" = "${pkgs.exa}/bin/exa -T";
+      }
+      // lib.optionalAttrs (mcfg.wm.enable && ucfg.wm.enable && ucfg.wm.term.enable)
+        { "icat" = "kitty +kitten icat"; };
+    };
+  };
 }
--- modules/shell/git/default.nix
+++ modules/shell/git/default.nix
@@ -1,11 +1,12 @@
-{ machine = { ... }: {};
+{
+  machine = { ... }: {};
 
-  users = { config, lib, ... }:
-    let cfg = config.m4ch1n3.shell.git;
+  users = { lib, ucfg, ... }:
+    let
+      cfg = ucfg.shell.git;
+    in {
+      options.m4ch1n3.shell.git.enable = lib.mkOptBool true;
 
-    in { options.m4ch1n3.shell.git =
-           { enable = lib.mkDisableOption "git"; };
-
-         config.programs.git.enable = cfg.enable;
-       };
+      config.programs.git.enable = cfg.enable;
+    };
 }
