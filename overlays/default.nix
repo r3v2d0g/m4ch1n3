5e8cf9ecc4a2684e4498e3e7fef56fb154925a4b
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-10 18:34:53 +0100

    overlays: refactor

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,34 +6,26 @@
 
 self: super:
 
-{ emacsGcc = super.emacsGcc
-    .overrideAttrs
-    (prev:
-      { patches =
-          (prev.patches or [])
-          ++ [ ./patches/emacsGcc/0001-emacsgcc-fix-doom-emacs-install.patch ];
-      }
-    );
+{
+  emacsGcc = super.emacsGcc .overrideAttrs (prev: {
+    patches = (prev.patches or [])
+              ++ [ ./patches/emacsGcc/0001-emacsgcc-fix-doom-emacs-install.patch ];
+  });
 
-  protonmail-bridge = self.callPackage ./protonmail-bridge
-    { source = proton-bridge; };
+  protonmail-bridge = self.callPackage ./protonmail-bridge { source = proton-bridge; };
 
-  rtl8723de = self.callPackage ./rtl8723de
-    { source = rtl8723de;
-      kernel = self.linux;
-    };
+  rtl8723de = self.callPackage ./rtl8723de {
+    source = rtl8723de;
+    kernel = self.linux;
+  };
 
-  waybar = super.waybar
-    .overrideAttrs
-    (prev:
-      { src = waybar;
-        version = "0.9.5";
+  waybar = super.waybar .overrideAttrs (prev: {
+    src = waybar;
+    version = "0.9.5";
 
-        patches =
-          (prev.patches or [])
-          ++ [ ./patches/waybar/0001-no-exclusive-zone-for-overlay.patch ];
+    patches = (prev.patches or [])
+              ++ [ ./patches/waybar/0001-no-exclusive-zone-for-overlay.patch ];
 
-        mesonFlags = prev.mesonFlags ++ [ "-Dsndio=disabled" ];
-      }
-    );
+    mesonFlags = prev.mesonFlags ++ [ "-Dsndio=disabled" ];
+  });
 }
