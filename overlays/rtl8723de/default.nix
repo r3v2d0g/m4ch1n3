b16bddfd0a7d1cba57f7d024fd40e8927c8abf12
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-20 16:24:24 +0100

    overlays/rtl8723de: refactor

--- overlays/rtl8723de/default.nix
+++ overlays/rtl8723de/default.nix
@@ -1,23 +1,19 @@
 { bc, kernel, source, stdenv }:
 
-stdenv.mkDerivation
-  { name = "rtl8723de";
-    src = source;
+stdenv.mkDerivation {
+  name = "rtl8723de";
+  src = source;
 
-    hardeningDisable = [ "pic" ];
-    enableParallelBuilding = true;
-    skipStrip = true;
+  hardeningDisable = [ "pic" ];
+  enableParallelBuilding = true;
+  skipStrip = true;
 
-    nativeBuildInputs =
-      kernel.moduleBuildDependencies
-      ++ [ bc ];
+  nativeBuildInputs = kernel.moduleBuildDependencies ++ [ bc ];
 
-    makeFlags =
-      [ "KSRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build" ];
+  makeFlags = [ "KSRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build" ];
 
-    installPhase =
-      ''
-        install -vDm 644 8723de.ko \
-          "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/realtek/rtl8723de.ko"
-      '';
-  }
+  installPhase = ''
+    install -vDm 644 8723de.ko \
+      "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/realtek/rtl8723de.ko"
+  '';
+}
