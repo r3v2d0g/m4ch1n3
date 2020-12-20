ccfae069385998f9bdf864758494ca81743ed05a
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-20 18:42:29 +0100

    lib: add 'callOrImport' function

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,15 @@
 { lib }@args:
 
+with lib;
+
 lib
 // (import ./attrsets args)
 // (import ./options args)
 // (import ./strings args)
 // { term = import ./term args; }
+// {
+  callOrImport = fnOrPath:
+    if isFunction fnOrPath
+    then fnOrPath
+    else import fnOrPath;
+}
