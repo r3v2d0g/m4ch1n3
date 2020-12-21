ee48c72d8523abbf4ef09fd9e6bc74fa7d266425
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-21 18:36:52 +0100

    modules/dev/rust: install 'pkgs.wasm-pack'

--- modules/dev/rust/default.nix
+++ modules/dev/rust/default.nix
@@ -7,6 +7,10 @@
       enable = mcfg.dev.enable && ucfg.dev.enable;
     in {
       options.m4ch1n3.dev.rust = lib.optionalAttrs enable { enable = lib.mkOptBool true; };
-      config.home.packages = lib.mkIf (enable && cfg.enable) [ pkgs.rustup ];
+
+      config.home.packages = lib.mkIf (enable && cfg.enable) [
+        pkgs.rustup
+        pkgs.wasm-pack
+      ];
     };
 }
