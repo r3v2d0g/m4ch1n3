ff37c36afdc27f136d59b53a0fa94bb0e7985a61
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-21 18:37:55 +0100

    modules/dev: install 'pkgs.binutils'

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -1,8 +1,15 @@
 {
   machine = { lib, ... }: { options.m4ch1n3.dev.enable = lib.mkOptBool false; };
 
-  users = { lib, mcfg, ... }:
+  users = { lib, mcfg, ucfg, pkgs, ... }:
     let
+      cfg = ucfg.dev;
       enable = mcfg.dev.enable;
-    in { options.m4ch1n3.dev = lib.optionalAttrs enable { enable = lib.mkOptBool true; }; };
+    in {
+      options.m4ch1n3.dev = lib.optionalAttrs enable { enable = lib.mkOptBool true; };
+
+      config.home.packages = lib.mkIf (enable && cfg.enable) [
+        pkgs.binutils
+      ];
+    };
 }
