f1761cba4d676c2675b0f09e42e26f0fe356ad48
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-26 14:58:49 +0100

    modules/dev: install and configure 'pkg-config'

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -1,15 +1,21 @@
 {
   machine = { lib, ... }: { options.m4ch1n3.dev.enable = lib.mkOptBool false; };
 
-  users = { lib, mcfg, ucfg, pkgs, ... }:
+  users = { config, lib, mcfg, ucfg, pkgs, ... }:
     let
       cfg = ucfg.dev;
       enable = mcfg.dev.enable;
     in {
       options.m4ch1n3.dev = lib.optionalAttrs enable { enable = lib.mkOptBool true; };
 
-      config.home.packages = lib.mkIf (enable && cfg.enable) [
-        pkgs.binutils
-      ];
+      config.home = lib.mkIf (enable && cfg.enable) {
+        packages = [
+          pkgs.binutils
+          pkgs.pkg-config
+        ];
+
+        sessionVariables."PKG_CONFIG_PATH" =
+          "/run/current-system/sw/lib/pkgconfig:${config.home.profileDirectory}/lib/pkgconfig";
+      };
     };
 }
