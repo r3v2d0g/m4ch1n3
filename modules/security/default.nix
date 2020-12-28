8b3780a4ebad5a24bcffc5226b8f823500054864
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-28 14:53:05 +0100

    modules/security: setup openssl

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -1,5 +1,12 @@
 {
   machine = { ... }: {};
 
-  users = { ... }: {};
+  users = { pkgs, ... }: {
+    home.packages = [ pkgs.openssl ];
+
+    home.sessionVariables = {
+      "OPENSSL_INCLUDE_DIR" = "${pkgs.openssl.dev}/include";
+      "OPENSSL_LIB_DIR" = "${pkgs.openssl.out}/lib";
+    };
+  };
 }
