011dc84f49bb231d2d776e6f51f2909d46ce9031
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-12-28 14:53:56 +0100

    modules/editor/emacs/packages.el: enable 'atomic-chrome'

--- modules/editor/emacs/packages.el
+++ modules/editor/emacs/packages.el
@@ -9,7 +9,7 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
-;;(package! atomic-chrome)
+(package! atomic-chrome)
 (package! ox-gfm)
 
 ;; To install a package directly from a remote git repo, you must specify a
