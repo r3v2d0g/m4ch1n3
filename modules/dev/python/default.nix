2639ee3fc31e2516a6c1a70135d8659d7a216412
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2021-01-04 16:08:48 +0100

    modules/dev/python: install python packages

--- modules/dev/python/default.nix
+++ modules/dev/python/default.nix
@@ -13,6 +13,15 @@
         packages = lib.mkOptAny pkgs.python38Packages;
       };
 
-      config.home.packages = lib.mkIf (enable && cfg.enable) [ cfg.package ];
+      config.home.packages = lib.mkIf (enable && cfg.enable) [
+        cfg.package
+
+        cfg.packages.fastecdsa
+        cfg.packages.pip
+        cfg.packages.python_magic
+        cfg.packages.pygraphviz
+        cfg.packages.secp256k1
+        cfg.packages.setuptools
+      ];
     };
 }
