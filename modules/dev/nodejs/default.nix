9d007cfb9ae1dd600c5c1a32fe5b2e6649ee2aa7
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2021-02-22 13:19:31 +0100

    modules/dev/nodejs: use nodejs v15.x

--- modules/dev/nodejs/default.nix
+++ modules/dev/nodejs/default.nix
@@ -6,12 +6,14 @@
       cfg = ucfg.dev.nodejs;
       enable = mcfg.dev.enable && ucfg.dev.enable
                && cfg.enable;
+
+      nodejs = pkgs.nodejs-15_x;
+      yarn = pkgs.yarn.overrideAttrs (_: { buildInputs = [ nodejs ]; });
     in {
       options.m4ch1n3.dev.nodejs.enable = lib.mkOptBool true;
       config.home.packages = lib.mkIf enable [
-        pkgs.nodejs
-        pkgs.nodePackages.npm
-        pkgs.yarn
+        nodejs
+        yarn
       ];
     };
 }
