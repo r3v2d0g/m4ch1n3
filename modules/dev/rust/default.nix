fb2a6a505481ec5ab1faa15f46fae47ce706cfcc
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2021-02-22 13:19:04 +0100

    modules/dev/rust: update rust and fix rust-analyzer

--- modules/dev/rust/default.nix
+++ modules/dev/rust/default.nix
@@ -9,17 +9,21 @@
 
       rust-nightly = (pkgs.rustChannelOf {
         channel = "nightly";
-        date = "2021-01-20";
-        sha256 = "0a9a29grqy2vvy4ygixgy0spm8r8pvavh359fllgqsvacm55pi81";
+        date = "2021-02-17";
+        sha256 = "1lfb9w6hkfff8mi238izqnawbm8jg2kimcwkg1px714hp39xwryq";
       }).rust.override {
         extensions = ["rust-src"];
       };
     in {
       options.m4ch1n3.dev.rust.enable = lib.mkOptBool true;
 
-      config.home.packages = lib.mkIf enable [
-        rust-nightly
-        pkgs.wasm-pack
-      ];
+      config.home = lib.mkIf enable {
+        packages = [
+          rust-nightly
+          pkgs.wasm-pack
+        ];
+
+        sessionVariables."RUST_SRC_PATH" = "${rust-nightly}/lib/rustlib/src/rust/library";
+      };
     };
 }
