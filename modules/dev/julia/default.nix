3e6c513243abcdf2e7e5810c2ee4352eae434bf3
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2021-05-22 15:16:53 +0200

    dev/julia: disable by default

--- modules/dev/julia/default.nix
+++ modules/dev/julia/default.nix
@@ -7,7 +7,7 @@
       enable = mcfg.dev.enable && ucfg.dev.enable
         && cfg.enable;
     in {
-      options.m4ch1n3.dev.julia.enable = lib.mkOptBool true;
+      options.m4ch1n3.dev.julia.enable = lib.mkOptBool false;
       config.home.packages = lib.mkIf enable [ pkgs.julia ];
     };
 }
